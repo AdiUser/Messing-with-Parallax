b361a4cc95b55a3a43ca7524f4570e15635f375e
Aditya Saxena <adityasaxena602@gmail.com>
2017-06-06 22:45:28 +0530

    Animating working [hacked] not responsive

--- JSmain.js
+++ JSmain.js
@@ -1,43 +1,36 @@
 (function() {
 
-  'use strict';
+    'use strict';
 
-  // define variables
+    // define variables
 
-  /* the arry of 'li' were not populating due to fast loading or something or applied this ~hack~ instead; */
-  var items ;
-  window.addEventListener('load', function (e) {
-  		// body...
-  		var items_x = document.querySelectorAll(".plate_section li");
-  		items = items_x;
-  });
-  
+    /* the arry of 'li' were not populating due to fast loading or something or applied this ~hack~ instead; */
+    var items;
+    window.addEventListener('load', function(e) {
+        // body...
+        var items_x = document.querySelectorAll(".plate_section li");
+        items = items_x;
+    });
 
-
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function callbackFunc() {
- 
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return ( rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth)) ;
     }
-  }
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
+    function callbackFunc() {
+
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
 
 })();
